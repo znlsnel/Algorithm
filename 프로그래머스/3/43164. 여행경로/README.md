078673e7da85d57a6111d749a3575cfbc8f5b50b
UdaeB <105254759+znlsnel@users.noreply.github.com>
2025-05-13 00:48:01 +0900

    [level 3] Title: 여행경로, Time: 6.96 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=csharp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.03 ms
+메모리: 32 MB, 시간: 6.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 13일 00:47:30
+2025년 05월 13일 00:48:00
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.cs
+++ 프로그래머스/3/43164. 여행경로/여행경로.cs
@@ -1,8 +1,58 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Solution {
-    public string[] solution(string[,] tickets) {
-        string[] answer = new string[] {};
-        return answer;
+public class Solution 
+{
+    private List<(string, string)> Ticket = new List<(string, string)>();
+    private Dictionary<string, List<int>> indices = new Dictionary<string, List<int>>();
+    
+    private List<string> answer = new List<string>();
+    private bool[] visited;
+    
+    private void DFS(string node, int cnt)
+    {
+        answer.Add(node);
+        
+        if (answer.Count == Ticket.Count+1 || !indices.ContainsKey(node))
+            return;
+        
+        foreach (int index in indices[node])
+        {
+            if (visited[index])
+                continue;
+
+            visited[index] = true;
+            
+            string nxt = Ticket[index].Item2;
+            DFS(nxt, cnt + 1);
+
+            if (answer.Count <= Ticket.Count)
+            {
+                visited[index] = false;
+                answer.RemoveAt(answer.Count - 1);
+            }
+        } 
+    }
+    
+    public string[] solution(string[,] tickets) 
+    {
+        visited = new bool[tickets.GetLength(0)];
+        
+        for (int i = 0; i < tickets.GetLength(0); i++)
+            Ticket.Add((tickets[i, 0], tickets[i, 1]));
+            
+        Ticket.Sort();
+        for (int i = 0; i < Ticket.Count; i++)
+        {
+            if (!indices.ContainsKey(Ticket[i].Item1))
+                indices.Add(Ticket[i].Item1, new List<int>());
+            
+            indices[Ticket[i].Item1].Add(i);
+        }
+        
+        DFS("ICN", 0);
+        
+        return answer.ToArray();
     }
 }
