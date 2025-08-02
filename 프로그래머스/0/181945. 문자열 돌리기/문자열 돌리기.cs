966166401011cba05346cd3a910997e0b71c8f52
UdaeB <105254759+znlsnel@users.noreply.github.com>
2025-08-02 17:57:40 +0900

    [level 0] Title: 문자열 돌리기, Time: 18.77 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        
+        s = Console.ReadLine();
+        
+        foreach (char c in s)
+        {
+            Console.WriteLine(c);
+        }
+
+    }
+}
