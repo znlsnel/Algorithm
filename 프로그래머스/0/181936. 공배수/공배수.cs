ffb20c7da26e08175d675504e21ad495113ff8d2
UdaeB <105254759+znlsnel@users.noreply.github.com>
2025-10-04 16:52:20 +0900

    [level 0] Title: 공배수, Time: 0.19 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        return number % n == 0 && number % m == 0 ? 1 : 0;
+    }
+}
