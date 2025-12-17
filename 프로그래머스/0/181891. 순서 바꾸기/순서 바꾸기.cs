ce6209ca9eb97fcad5f1fe0da872f2eef0bf4501
UdaeB <105254759+znlsnel@users.noreply.github.com>
2025-12-17 22:29:53 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.87 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<int> answer = new List<int>();
+        
+        for (int i = n; i < num_list.Length; i++)
+            answer.Add(num_list[i]);
+        
+        for (int i = 0; i < n; i++)
+            answer.Add(num_list[i]);
+        
+        return answer.ToArray();
+    }
+}
