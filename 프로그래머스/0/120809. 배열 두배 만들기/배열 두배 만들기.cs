ab868c578a20a554aaa84f8bf721c9630af374cf
UdaeB <105254759+znlsnel@users.noreply.github.com>
2025-12-19 20:05:44 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.16 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] numbers) {
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] *= 2;
+        return numbers;
+    }
+}
