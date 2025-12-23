a43835f1ddc80c70331872d35291c7e27b025506
UdaeB <105254759+znlsnel@users.noreply.github.com>
2025-12-23 22:05:04 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.31 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int[,] arr) {
+        int n = arr.GetLength(0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i, j] != arr[j, i]) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+}
